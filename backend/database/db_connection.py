aca6ca09a58c5dbb0919a7f4254711015bf74e9e
Shankar Reddy Mopur <mopurshankareddy@gmail.com>
2025-09-29 13:11:00 +0530

    Update db_connection.py

--- backend/database/db_connection.py
+++ backend/database/db_connection.py
@@ -4,8 +4,8 @@ from psycopg2.extras import RealDictCursor
 def get_db_connection():
     conn = psycopg2.connect(
         dbname="disaster_db",
-        user="your_db_user",
-        password="your_password",
+        user="Database",           
+        password="Shankar@0747",       
         host="localhost",
         port=5432
     )
